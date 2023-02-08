9a449649850201a5fb3cf835bee85c403265e045
anggicute44 <117901030+anggicute44@users.noreply.github.com>
2023-02-08 19:22:09 +0700

    Add files via upload

---
+++ 3_D_71220917.py
@@ -0,0 +1,29 @@
+
+
+#soal 3
+print()
+  
+lebar_belah_ketupat = int(input('masukkan angka: '))
+print()
+  
+for i in range(lebar_belah_ketupat):
+  for j in range(lebar_belah_ketupat-i):
+    print(' ',end='')
+      
+  for k in range(i+1):
+    print('* ',end='')
+  print()
+ 
+for i in range(1,lebar_belah_ketupat):
+  for j in range(i+1):
+    print(' ',end='')
+      
+  for k in range(lebar_belah_ketupat-i):
+    print('* ',end='')
+  print()
+
+
+
+
+
+
